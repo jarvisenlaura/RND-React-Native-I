94ebc08bc44c69d6b553e22e9eb9d5d1a4c301cc
Järvisen Laura <jarvisenlaura@gmail.com>
2019-11-25 12:26:34 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, ScrollView, SafeAreaView } from "react-native";
+import { View, ScrollView } from "react-native";
 import { MyAppText, textStyles } from "./MyAppText";
 
 const styles = {
@@ -19,78 +19,71 @@ const styles = {
 };
 
 const App = () => (
-  <SafeAreaView>
+  <View style={styles.container}>
+    <MyAppText style={styles.otsikko(2.5)}>
+      Otsikko 1
+    </MyAppText>
+    <MyAppText style={styles.otsikko(2)}>
+      Otsikko 2
+    </MyAppText>
+    <MyAppText style={styles.otsikko(1.75)}>
+      Otsikko 3
+    </MyAppText>
+    <MyAppText style={styles.otsikko(1.5)}>
+      Otsikko 4
+    </MyAppText>
+    <MyAppText style={styles.otsikko(1.25)}>
+      Otsikko 5
+    </MyAppText>
+    <MyAppText style={styles.otsikko(1)}>
+      Otsikko 6
+    </MyAppText>
+    <MyAppText>
+      Leipäteksti
+    </MyAppText>
+    <MyAppText style={{fontStyle: "italic"}}>
+      Kursivoitu
+    </MyAppText>
+    <MyAppText style={{fontWeight: "bold"}}>
+      Lihavoitu
+    </MyAppText>
+    <MyAppText style={{textDecorationLine: "underline"}}>
+      Alaviiva
+    </MyAppText>
+    <MyAppText>
+      {"\n"}
+    </MyAppText>
     <ScrollView>
-      <View style={styles.container}>
-          <MyAppText style={styles.otsikko(2.5)}>
-            Otsikko 1
-          </MyAppText>
-          <MyAppText style={styles.otsikko(2)}>
-            Otsikko 2
-          </MyAppText>
-          <MyAppText style={styles.otsikko(1.75)}>
-            Otsikko 3
-          </MyAppText>
-          <MyAppText style={styles.otsikko(1.5)}>
-            Otsikko 4
-          </MyAppText>
-          <MyAppText style={styles.otsikko(1.25)}>
-            Otsikko 5
-          </MyAppText>
-          <MyAppText style={styles.otsikko(1)}>
-            Otsikko 6
-          </MyAppText>
-          
-          <MyAppText>
-            {"\n"}
-          </MyAppText>
-
-          <MyAppText>
-            Leipäteksti
-          </MyAppText>
-          <MyAppText style={{fontStyle: "italic"}}>
-            Kursivoitu
-          </MyAppText>
-          <MyAppText style={{fontWeight: "bold"}}>
-            Lihavoitu
-          </MyAppText>
-          <MyAppText style={{textDecorationLine: "underline"}}>
-            Alaviiva
-          </MyAppText>
-          <MyAppText>
-            {"\n"}
-          </MyAppText>
-          <MyAppText style={styles.otsikko(2)}>Iljettävä Imulima-rock </MyAppText>
-          <MyAppText>Iljettävä Imulimanäyte </MyAppText>
-          <MyAppText>leipoi kakun josson myrkkysienitäyte. </MyAppText>
-          <MyAppText>Säilytti sen kellarissa jäissä </MyAppText>
-          <MyAppText>tarjotakseen Herbertille häissä. </MyAppText>
-          <MyAppText>
-            {"\n"}
-          </MyAppText>
-          <MyAppText>Oi ei! Oi ei! </MyAppText>
-          <MyAppText>
-            {"\n"}
-          </MyAppText>
-          <MyAppText>Törkein vieras koko kakun nieli: </MyAppText>
-          <MyAppText>paisui polla ja mustui kieli. </MyAppText>
-          <MyAppText>Herbertille tuli paha mieli, </MyAppText>
-          <MyAppText>kun kiitoksen sijaan </MyAppText>
-          <MyAppText>vierat röyhtäili ja pieri.</MyAppText>
-          <MyAppText>
-            {"\n"}
-          </MyAppText>
-          <MyAppText>Oi ei! Oi ei! </MyAppText>
-          <MyAppText>
-            {"\n"}
-          </MyAppText>
-          <MyAppText style={{fontStyle: "italic"}}>Laura Ruohonen, Yökyöpeli </MyAppText>
-          <MyAppText>
-            {"\n"}
-          </MyAppText>
-        </View>  
-      </ScrollView> 
-    </SafeAreaView> 
+      <MyAppText style={styles.otsikko(2)}>Iljettävä Imulima-rock </MyAppText>
+      <MyAppText>Iljettävä Imulimanäyte </MyAppText>
+      <MyAppText>leipoi kakun joss'on myrkkysienitäyte. </MyAppText>
+      <MyAppText>Säilytti sen kellarissa jäissä </MyAppText>
+      <MyAppText>tarjotakseen Herbertille häissä. </MyAppText>
+      <MyAppText>
+        {"\n"}
+      </MyAppText>
+      <MyAppText>Oi ei! Oi ei! </MyAppText>
+      <MyAppText>
+        {"\n"}
+      </MyAppText>
+      <MyAppText>Törkein vieras koko kakun nieli: </MyAppText>
+      <MyAppText>paisui polla ja mustui kieli. </MyAppText>
+      <MyAppText>Herbertille tuli paha mieli, </MyAppText>
+      <MyAppText>kun kiitoksen sijaan </MyAppText>
+      <MyAppText>vierat röyhtäili ja pieri.</MyAppText>
+      <MyAppText>
+        {"\n"}
+      </MyAppText>
+      <MyAppText>Oi ei! Oi ei! </MyAppText>
+      <MyAppText>
+        {"\n"}
+      </MyAppText>
+      <MyAppText style={{fontStyle: "italic"}}>Laura Ruohonen, Yökyöpeli </MyAppText>
+      <MyAppText>
+        {"\n"}
+      </MyAppText>
+    </ScrollView> 
+  </View>  
 );
 
 export default App;
