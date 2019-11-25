4f1fcf103f7e2250c817b50a48362ed08f6ddd2f
Järvisen Laura <jarvisenlaura@gmail.com>
2019-11-25 17:08:25 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,60 +1,28 @@
-import React from "react";
-import { View } from "react-native";
-import { MyAppText, textStyles } from "./MyAppText";
+import React, { Component } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
 
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  otsikko: (skaala) => {
-    const fontSize = textStyles.fontSize * skaala;
-
-    return {
-      lineHeight: fontSize * 1,
-      marginBottom: 12,
-      fontWeight: "500",
-      fontSize
-    };
+export default class App extends Component {
+  render() {
+    return (
+        <View style={{
+          justifyContent: 'center',
+          alignItems: 'center'}
+          }>
+          <Image    
+            style={{height: 160}}
+            source={require('./assets/images/mobiilikuva.png')}
+          />
+          <Image 
+            style={{height: 300, width: 400 }}
+            resizeMode='contain'
+            source={{ uri: 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' }}
+          />
+          <Image 
+            style={{height: 250, width: 400 }}
+            resizeMode='contain'
+            source={{ uri: 'https://images.unsplash.com/photo-1548403119-4f9f60f7c064?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' }}
+          />
+        </View>
+    );
   }
-};
-
-const App = () => (
-  <View style={styles.container}>
-    <MyAppText style={styles.otsikko(2.5)}>
-      Otsikko 1
-    </MyAppText>
-    <MyAppText style={styles.otsikko(2)}>
-      Otsikko 2
-    </MyAppText>
-    <MyAppText style={styles.otsikko(1.75)}>
-      Otsikko 3
-    </MyAppText>
-    <MyAppText style={styles.otsikko(1.5)}>
-      Otsikko 4
-    </MyAppText>
-    <MyAppText style={styles.otsikko(1.25)}>
-      Otsikko 5
-    </MyAppText>
-    <MyAppText style={styles.otsikko(1)}>
-      Otsikko 6
-    </MyAppText>
-    
-    <MyAppText>
-      {"\n"}
-    </MyAppText>
-
-    <MyAppText>
-      Leipäteksti
-    </MyAppText>
-    <MyAppText style={{fontWeight: "bold"}}>
-      Lihavoitu
-    </MyAppText>
-    <MyAppText style={{textDecorationLine: "underline"}}>
-      Alaviiva
-    </MyAppText>
-  </View>
-);
-
-export default App;
+}
