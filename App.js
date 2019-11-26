ec28db2fa00fc3cf288cf6258318cae87d96df54
Järvisen Laura <jarvisenlaura@gmail.com>
2019-11-26 14:30:43 +0200

    Update App.js

--- App.js
+++ App.js
@@ -20,57 +20,45 @@ const styles = {
 
 const App = () => (
   <View style={styles.container}>
-    <MyAppText style={styles.otsikko(2.5)}>
-      Otsikko 1
-    </MyAppText>
-    <MyAppText style={styles.otsikko(2)}>
-      Otsikko 2
-    </MyAppText>
-    <MyAppText style={styles.otsikko(1.75)}>
-      Otsikko 3
-    </MyAppText>
-    <MyAppText style={styles.otsikko(1.5)}>
-      Otsikko 4
-    </MyAppText>
-    <MyAppText style={styles.otsikko(1.25)}>
-      Otsikko 5
-    </MyAppText>
-    <MyAppText style={styles.otsikko(1)}>
-      Otsikko 6
-    </MyAppText>
-    
-    <MyAppText>
-      {"\n"}
-    </MyAppText>
-
-    <MyAppText>
-      Leipäteksti
-    </MyAppText>
-    <MyAppText style={{fontStyle: "italic"}}>
-      Kursivoitu
-    </MyAppText>
-    <MyAppText style={{fontWeight: "bold"}}>
-      Lihavoitu
-    </MyAppText>
-    <MyAppText style={{textDecorationLine: "underline"}}>
-      Alaviiva
-    </MyAppText>
-    <MyAppText>
-      {"\n"}
-    </MyAppText>
-    <MyAppText>
-      <MyAppText>Kun teksti useammalla rivillä on kääritty isomman Text-</MyAppText>
-      <MyAppText>komponentin sisään, sen juokseminen riviltä toiselle sujuu paremmin.</MyAppText>
-    </MyAppText>
-    <MyAppText>
-      {"\n"}
-    </MyAppText>
-    <View>
-      <MyAppText>Kun teksti useammalla rivillä on kääritty isomman View-komponentin </MyAppText>
-      <MyAppText>sisään, rivivaihdot ei suju yhtä hyvin.</MyAppText>
-    </View>
-    <MyAppText>Lisää tähän muutama rivi omaa tekstiä... </MyAppText>
-  </View>
+    <MyAppText 
+      style={styles.otsikko(2.5)}
+      text="Otsikko 1" 
+      />
+    <MyAppText
+      style={styles.otsikko(2)}
+      text="Otsikko 2"
+    />
+    <MyAppText 
+      style={styles.otsikko(1.75)}
+      text="Otsikko 3" 
+    />
+    <MyAppText 
+      style={styles.otsikko(1.5)}
+      text="Otsikko 4" 
+    />
+    <MyAppText 
+      style={styles.otsikko(1.25)}
+      text="Otsikko 5" 
+    />  
+    <MyAppText 
+      text="Leipäteksti" 
+    />
+    <MyAppText 
+      style={{fontStyle: "italic"}}
+      text="Kursivoitu" 
+    />
+    <MyAppText
+      style={{fontWeight: "bold"}}
+      text="Lihavoitu"
+    />
+    <MyAppText
+      style={{textDecorationLine: "underline"}}
+      text="Alaviiva"
+    />
+    <MyAppText
+      text={"\n"}
+    />    
+  </View>  
 );
 
 export default App;
