31b8884b754398cd5878bd19e9a5247dd457cfee
Järvisen Laura <jarvisenlaura@gmail.com>
2019-12-11 15:55:19 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 
-export default class App extends Component {
-  render() {
-    return (
-        <View style={{
-          justifyContent: 'center',
-          alignItems: 'center'}
-          }>
-          <Image    
-            style={{height: 160}}
-            source={require('./assets/images/mobiilikuva.png')}
-          />
-          <Image 
-            style={{height: 300, width: 400 }}
-            resizeMode='contain'
-            source={{ uri: 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' }}
-          />
-          <Image 
-            style={{height: 250, width: 400 }}
-            resizeMode='contain'
-            source={{ uri: 'https://images.unsplash.com/photo-1548403119-4f9f60f7c064?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' }}
-          />
-        </View>
-    );
-  }
-}
+const App =() => (
+  <View style={{
+    justifyContent: 'center',
+    alignItems: 'center'}
+  }>
+    <Image    
+      style={{height: 180}}
+      source={require('./assets/images/mobiilikuva.png')}
+    />
+    <Image 
+      style={{height: 300, width: 400 }}
+      resizeMode='contain'
+       source={{ uri: 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' }}
+    />
+    <Image 
+      style={{height: 250, width: 400 }}
+      resizeMode='contain'
+      source={{ uri: 'https://images.unsplash.com/photo-1548403119-4f9f60f7c064?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' }}
+    />
+  </View>
+);
+export default App;
